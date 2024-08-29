c8509faddc5d654e943509e3af9f0d056597ab31
Fujisawa Yuki <g031y148yuki@gmail.com>
2024-08-29 21:42:02 +0900

    Refactor: 不要なタグの削除

--- src/controllers/hello_controller.js
+++ src/controllers/hello_controller.js
@@ -4,10 +4,10 @@ export default class HelloController extends Controller {
   static targets = ["output"];
 
   connect() {
-    this.outputTarget.textContent = "こんにちは、Stimulus!";
+    this.outputTarget.textContent = "ここが変わるよ!";
   }
 
   greet() {
-    this.outputTarget.textContent = "こんにちは、世界!";
+    this.outputTarget.textContent = "Hello, Stimulus!";
   }
 }
