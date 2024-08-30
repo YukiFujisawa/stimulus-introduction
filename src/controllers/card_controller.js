92ed79be365162a3d22807c3d951a6f4c0a3099a
Fujisawa Yuki <g031y148yuki@gmail.com>
2024-08-30 21:49:57 +0900

    Add: indexページ

--- src/controllers/card_controller.js
+++ src/controllers/card_controller.js
@@ -11,26 +11,29 @@ export default class extends Controller {
   ];
 
   connect() {
-    this.loadCards();
+    if (this.isIndexPage()) {
+      this.loadCards();
+    }
+    this.addBackButton();
+  }
+
+  isIndexPage() {
+    return (
+      window.location.pathname.endsWith("index.html") ||
+      window.location.pathname.endsWith("/")
+    );
   }
 
   loadCards() {
     const cards = [
       {
-        image: "path/to/profile-image.jpg",
-        title: "タイトル",
-        description: "説明",
-        category: "カテゴリ",
-        demoLink: "#",
+        image: "assets/img1.png",
+        title: "Hello World",
+        description: "Stimulusの基本的な使い方を学ぶシンプルなデモ",
+        category: "Stimulus 基礎",
+        demoLink: "src/view/hello.html",
       },
-      {
-        image: "path/to/photo-book-image.jpg",
-        title: "タイトル",
-        description: "説明",
-        category: "カテゴリ",
-        demoLink: "#",
-      },
-      // 他のカードデータをここに追加
+      // 他のカードデータ...
     ];
 
     cards.forEach((card) => this.createCard(card));
@@ -48,5 +51,21 @@ export default class extends Controller {
     clone.querySelector('[data-card-target="demo"]').href = data.demoLink;
 
     this.element.appendChild(clone);
+  }
+
+  addBackButton() {
+    if (!this.isIndexPage()) {
+      const backButton = document.createElement("a");
+      backButton.href = "/";
+      backButton.className =
+        "fixed top-4 left-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition flex items-center";
+      backButton.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd" />
+                </svg>
+                戻る
+            `;
+      document.body.appendChild(backButton);
+    }
   }
 }
